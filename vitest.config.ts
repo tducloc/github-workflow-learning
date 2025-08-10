7084e8e8bc03a14ef76f6a3a43682d70691882d2
Loc Tran <ducloc0009@gmail.com>
2025-08-10 21:08:11 +0700

    chore: Update vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    css: true,
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*"],
+    },
+    include: ["src/**/*.{test,spec}.{ts,tsx}"],
+  },
+});
